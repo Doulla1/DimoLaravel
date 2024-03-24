49dca08b8f0d4ab5b6ace0a5e3094c8b2bc9c891
Abdoulaye <dialloabdoul99c@gmail.com>
2024-03-24 03:27:17 +0100

    Adding courses routes and methods

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -2,9 +2,182 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
-    //
+    /**
+     * Get all courses
+     *
+     * @return JsonResponse
+     */
+    public function getAll()
+    {
+        try {
+            $courses = Course::all();
+            return response()->json(["cours"=>$courses], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                "status" => 0,
+                "message" => "An error occurred while getting courses"
+            ], 500);
+        }
+    }
+
+    /**
+     * Get a course by id
+     *
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function getById(int $id)
+    {
+        try {
+            $course = Course::find($id);
+            if ($course) {
+                return response()->json(["cours"=>$course], 200);
+            } else {
+                return response()->json([
+                    "message" => "Course not found"
+                ], 404);
+            }
+        } catch (Exception $e) {
+            return response()->json([
+                "message" => "An error occurred while getting course"
+            ], 500);
+        }
+    }
+
+    /**
+     * Get courses by teacher id
+     *
+     * @param int $teacherId
+     * @return JsonResponse
+     */
+    public function getByTeacherId(int $teacherId)
+    {
+        try {
+            $courses = Course::where('teacher_id', $teacherId)->get();
+            return response()->json(["cours"=>$courses], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                "message" => "An error occurred while getting courses"
+            ], 500);
+        }
+    }
+
+    /**
+     * Get all courses by connected teacher
+     *
+     * @return JsonResponse
+     */
+    public function getByConnectedTeacher()
+    {
+        try {
+            $teacherId = auth()->user()->id;
+            $courses = Course::where('teacher_id', $teacherId)->get();
+            return response()->json(["cours"=>$courses], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                "message" => "An error occurred while getting courses"
+            ], 500);
+        }
+    }
+
+
+
+    /**
+     * Create a course
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function create(Request $request)
+    {
+        try {
+            // Data validation
+            $request->validate([
+                'teacher_id' => 'required',
+                'classroom_id' => 'required',
+            ]);
+            // Check if the connected user is a teacher
+            if(!auth()->user()->hasRole('admin')){
+                $request->validate([
+                    'teacher_id' => 'required|in:'.auth()->user()->id
+                ]);
+            }
+            $course = Course::create($request->all());
+
+            return response()->json([
+                "data" => $course
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                "message" => "An error occurred while creating course"
+            ], 500);
+        }
+    }
+
+    /**
+     * Update a course
+     *
+     * @param Request $request
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function update(Request $request, int $id)
+    {
+        try {
+            // Data validation
+            $request->validate([
+                'teacher_id' => 'required',
+                'classroom_id' => 'required',
+            ]);
+            $course = Course::find($id);
+            if ($course &&
+                ($course->teacher_id == auth()->user()->id || auth()->user()->hasRole('admin'))) {
+                $course->update($request->all());
+                return response()->json([
+                    "data" => $course
+                ]);
+            } else {
+                return response()->json([
+                    "message" => "Course not found or you are not authorized to update it"
+                ], 404);
+            }
+        } catch (Exception $e) {
+            return response()->json([
+                "message" => "An error occurred while updating course"
+            ], 500);
+        }
+    }
+
+    /**
+     * Delete a course
+     *
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function delete(int $id)
+    {
+        try {
+            $course = Course::find($id);
+            if ($course&&
+                ($course->teacher_id == auth()->user()->id || auth()->user()->hasRole('admin'))) {
+                $course->delete();
+                return response()->json();
+            } else {
+                return response()->json([
+                    "message" => "Course not found or you are not authorized to delete it"
+                ], 404);
+            }
+        } catch (Exception $e) {
+            return response()->json([
+                "message" => "An error occurred while deleting course"
+            ], 500);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Routes accéssibles à tout le monde
@@ -14,16 +14,16 @@ Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLink
 
 // Routes accessibles à tout ceux qui sont connectés
 Route::middleware('auth:sanctum')->group(function () {
-    // Route pour récupérer les informations de l'utilisateur connecté
+    // Récupérer les informations de l'utilisateur connecté
     Route::get('/fetchUser',[UserController::class, 'getConnectedUser']);
 
-    // Route pour mettre à jour les informations de l'utilisateur connecté
+    // Mettre à jour les informations de l'utilisateur connecté
     Route::put('/updateUser', [UserController::class, 'updateConnectedUser']);
 
-    // Route pour mettre à jour le mot de passe de l'utilisateur connecté
+    // Mettre à jour le mot de passe de l'utilisateur connecté
     Route::put('/updateUserPassword', [UserController::class, 'updatePassword']);
 
-    // Route pour se déconnecter
+    // Se déconnecter
     Route::post('/logout', [LoginController::class, 'logout']);
 });
 
@@ -34,31 +34,53 @@ Route::middleware(["auth:sanctum", "checkrole:student"])->group(function () {
 
 // Routes accessibles aux teachers
 Route::middleware(["auth:sanctum", "checkrole:teacher"])->group(function () {
-    //
+    // Récupérer tous les cours
+    Route::get('/courses', [CourseController::class, 'getAll']);
+
+    // Créer un cours
+    Route::post('/courses', [CourseController::class, 'create']);
+
+    // Récupérer les cours du teacher connecté
+    Route::get('/courses/teacher', [CourseController::class, 'getByConnectedTeacher']);
+
+    // Mettre à jour un cours qui appartient au teacher connecté
+    Route::put('/courses/{id}', [CourseController::class, 'update']);
+
+    // Supprimer un cours qui appartient au teacher connecté
+    Route::delete('/courses/{id}', [CourseController::class, 'delete']);
 
 });
 
 // Routes accessibles qu'aux admins
 Route::middleware(["auth:sanctum", "checkrole:admin"])->group(function () {
-    // Route pour récupérer tous les utilisateurs
+    // Récupérer tous les utilisateurs
     Route::get ('/admin/users',[UserController::class, 'getAll']);
 
-    // Route pour récupérer un utilisateur par son id
+    // Récupérer un utilisateur par son id
     Route::get('/admin/users/{id}', [UserController::class, 'getUnique']);
 
-    // Route pour récupérer un utilisateur par son email
+    // Récupérer un utilisateur par son email
     Route::get('/admin/users/email/{email}', [UserController::class, 'getByEmail']);
 
-    // Route pour mettre à jour un utilisateur
+    // Mettre à jour un utilisateur
     Route::put('/admin/users/{id}', [UserController::class, 'update']);
 
     // Route pour supprimer un utilisateur
     Route::delete('/admin/users/{id}', [UserController::class, 'delete']);
 
-    // Route pour récupérer tous les rôles
+    // Récupérer tous les rôles
     Route::get('/admin/roles', [UserController::class, 'getRoles']);
 
     // Attribuer un rôle à un utilisateur
     Route::post('/admin/users/{id}/roles/{role_id}', [UserController::class, 'assignRole']);
+
+    // Récupérer tous les cours
+    Route::get('/admin/courses', [CourseController::class, 'getAll']);
+
+    // Récuperer un cours par son id
+    Route::get('/admin/courses/{id}', [CourseController::class, 'getById']);
+
+    // Récupérer les cours d'un teacher
+    Route::get('/admin/courses/teacher/{teacherId}', [CourseController::class, 'getByTeacherId']);
 
 });
